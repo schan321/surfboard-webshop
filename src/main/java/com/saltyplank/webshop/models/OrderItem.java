6fe24670fcf86ed0bd910451e858aa7ee9f02bf0
SchaneteaEsguerra <107695838+schan321@users.noreply.github.com>
2026-03-31 15:25:20 +0200

    created models and enums

--- src/main/java/com/saltyplank/webshop/models/OrderItem.java
+++ src/main/java/com/saltyplank/webshop/models/OrderItem.java
@@ -1,4 +1,61 @@
 package com.saltyplank.webshop.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+
+@Entity
+@Table(name = "order_items")
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Double unitPrice; // price at time of ordering
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Long id, Order order, Long productId, String productName, Double productPrice, Integer quantity) {
+        this.id = id;
+        this.order = order;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
